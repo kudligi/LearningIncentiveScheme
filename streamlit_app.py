ab89866fb5428a9c04cb9fbc6479f007919b1262
nihal kudligi <nihal1011ML@gmail.com>
2022-05-08 08:24:32 +0530

    Added some demo functionality

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 
 df = pd.DataFrame({
   'first column': [1, 2, 3, 4],
@@ -8,3 +9,30 @@ df = pd.DataFrame({
 
 st.write("Here's our first attempt at using data to create a table:")
 st.write(df)
+
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
+
+map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+st.map(map_data)
+
+x = st.slider('x')  # 👈 this is a widget
+st.write(x, 'squared is', x * x)
+
+st.text_input("Your name", key="name")
+
+# You can access the value at any point with:
+st.session_state.name
+
+if st.checkbox('Show dataframe'):
+    chart_data2 = pd.DataFrame(
+       np.random.randn(20, 3),
+       columns=['a', 'b', 'c'])
+
+    chart_data2

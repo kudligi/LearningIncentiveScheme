b47741bcc47f9c36f44b797148997a7a6ec47aa1
nihal kudligi <nihal1011ML@gmail.com>
2022-05-08 08:25:49 +0530

    added sidebar demo

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,3 +36,15 @@ if st.checkbox('Show dataframe'):
        columns=['a', 'b', 'c'])
 
     chart_data2
+    
+    
+add_selectbox = st.sidebar.selectbox(
+    'How would you like to be contacted?',
+    ('Email', 'Home phone', 'Mobile phone')
+)
+
+# Add a slider to the sidebar:
+add_slider = st.sidebar.slider(
+    'Select a range of values',
+    0.0, 100.0, (25.0, 75.0)
+)
